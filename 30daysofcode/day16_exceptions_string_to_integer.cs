8c8da894d45e0cfceec4fd52ef006d95e389bd72
Nathan Chere <git@nathanchere.com.au>
2016-10-06 13:34:48 +0200

    30DoC day 16 exception string to integer

--- 30daysofcode/day16_exceptions_string_to_integer.cs
+++ 30daysofcode/day16_exceptions_string_to_integer.cs
@@ -1,5 +1,20 @@
 // HackerRank "30 Days of Code" - Day 16 - Exceptions: String To Integer
 
-// ------[ Solution code begins here ]------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
-// ------[ Solution code ends here ]--------------------------------------------
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+        try
+        {
+          Console.WriteLine(Int32.Parse(S));
+        }
+        catch
+        {
+          Console.WriteLine("Bad String"); // ima smack your bum
+        }
+    }
+}
