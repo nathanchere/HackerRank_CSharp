2c21b2f88331f6460eb181af50f911b545e7fd94
Nathan Chere <git@nathanchere.com.au>
2016-10-06 13:20:43 +0200

    30DoC day 14 scope

--- 30daysofcode/day14_scope.cs
+++ 30daysofcode/day14_scope.cs
@@ -1,5 +1,35 @@
 // HackerRank "30 Days of Code" - Day 14 - Scope
 
+using System;
+using System.Linq;
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
 // ------[ Solution code begins here ]------------------------------------------
 
+  public Difference(int[] elements)
+  {
+    this.elements = elements;
+  }
+  public void computeDifference() =>
+    maximumDifference = elements.Max() - elements.Min();
+
 // ------[ Solution code ends here ]--------------------------------------------
+
+} // End of Difference Class
+
+class Solution {
+    static void Main(string[] args) {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
