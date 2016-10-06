6146c81dc2974549079a701693d44e914772af84
Nathan Chere <git@nathanchere.com.au>
2016-10-06 13:56:43 +0200

    30DoC day 21 generics

--- 30daysofcode/day21_generics.cs
+++ 30daysofcode/day21_generics.cs
@@ -7,6 +7,11 @@ public class Solution
 
 // ------[ Solution code begins here ]------------------------------------------
 
+  public static void printArray<T>(T[] values)
+  {
+    foreach(T value in values) Console.WriteLine(value);
+  }
+
 // ------[ Solution code ends here ]--------------------------------------------
 
   static void Main(string[] args)
